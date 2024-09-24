f1ccb4eeb38d7c30aa8c584244bcaeb373092799
wl <wolfgang.lambertz@protonmail.com>
2024-09-24 14:36:15 +0200

    Adds a new model.

---
+++ services/organizer/src/main/java/de/wolfware/badtourn/services/organizer/domain/Turnier.java
@@ -0,0 +1,15 @@
+package de.wolfware.badtourn.services.organizer.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record Turnier(
+        long id,
+        String name,
+        LocalDate startDatum,
+        LocalDate endDatum,
+        LocalDateTime anmeldeschluss,
+        List<Object> disziplinen, // todo implement specific class
+        List<Object> spieltage // todo implement specific class
+) {}
