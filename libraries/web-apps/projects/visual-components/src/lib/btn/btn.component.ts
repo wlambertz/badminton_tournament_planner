dd964b72d118ef2909d838b28f16b56556ca8c99
wl <wolfgang.lambertz@protonmail.com>
2024-10-25 21:08:08 +0200

    formatter and versions
    configures formatter and updates some versions

--- libraries/web-apps/projects/visual-components/src/lib/btn/btn.component.ts
+++ libraries/web-apps/projects/visual-components/src/lib/btn/btn.component.ts
@@ -1,22 +1,21 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon"
-import { NgIf, NgTemplateOutlet } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { NgIf, NgTemplateOutlet } from '@angular/common'
 
 @Component({
-  selector: "bt-comp-btn",
+  selector: 'bt-comp-btn',
   standalone: true,
   imports: [MatButtonModule, MatIconModule, NgTemplateOutlet, NgIf],
-  templateUrl: "./btn.component.html",
-  styleUrl: "./btn.component.scss",
+  templateUrl: './btn.component.html',
+  styleUrl: './btn.component.scss',
 })
-export class BtnComponent implements OnInit{
-  @Input() buttonType: string = 'secondary';
-  @Input() label?: string;
-  @Input() icon?: string;
-
+export class BtnComponent implements OnInit {
+  @Input() buttonType: string = 'secondary'
+  @Input() label?: string
+  @Input() icon?: string
 
   ngOnInit(): void {
-    if (!this.label) this.label = 'click me!';
+    if (!this.label) this.label = 'click me!'
   }
 }
