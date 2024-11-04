2c27ff2d58cbcbf85e703a012ba711d6e1dec4f5
wl <wolfgang.lambertz@protonmail.com>
2024-11-04 22:00:47 +0100

    Adds a Hamburger button

--- libraries/web-apps/projects/visual-components/src/lib/header/header.component.ts
+++ libraries/web-apps/projects/visual-components/src/lib/header/header.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core'
+import { NgClass } from '@angular/common'
 
 @Component({
   selector: 'bt-comp-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  protected isHamburgerOpen: boolean = false
+  protected toogleHamburger(): void {
+    document.getElementById('hamburger')?.classList.toggle('is-active')
+  }
+}
--- libraries/web-apps/projects/visual-components/src/public-api.ts
+++ libraries/web-apps/projects/visual-components/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of visual-components
  */
 
-export * from "./lib/visual-components.service";
-export * from "./lib/visual-components.component";
-export * from "./lib/btn/btn.component";
+export * from './lib/visual-components.service'
+export * from './lib/visual-components.component'
+export * from './lib/btn/btn.component'
+export * from './lib/header/header.component'
