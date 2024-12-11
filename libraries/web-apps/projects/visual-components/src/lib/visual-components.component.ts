55d60fa41d0212069e1fbcf3ebb50e8a58f0fd0c
wla <wolfgang.lambertz_external@yareto.de>
2024-12-11 18:02:50 +0100

    Fixes build

--- libraries/web-apps/projects/visual-components/src/lib/visual-components.component.ts
+++ libraries/web-apps/projects/visual-components/src/lib/visual-components.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'bt-comp-visual-components',
-  imports: [],
   template: ` <p>visual-components works!</p> `,
-  styles: ``,
   standalone: true,
+  styleUrls: ['./styles/styles.scss'],
 })
 export class VisualComponentsComponent {}
